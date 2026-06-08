eaa7e6fdd5098460588a5bb0650a388d42cc2134
Ola Ghattas <oghattas@hello-robot.com>
2026-06-08 15:27:25 -0700

    fix wheel names

--- stretch4_body/tools/stretch_line_sensor_viz_3d.py
+++ stretch4_body/tools/stretch_line_sensor_viz_3d.py
@@ -142,17 +142,17 @@ class LineSensorVisualizer3D:
         # URDF Transforms
         wheel_configs = [
             {
-                'name': 'wheel_0_link.STL',
+                'name': 'link_wheel_0.STL',
                 'xyz': [0.150688420258492, 0.0869999999999999, 0.0715000000000003],
                 'rpy': [-1.5707963267949, 0, -1.0471975511966]
             },
             {
-                'name': 'wheel_1_link.STL',
+                'name': 'link_wheel_1.STL',
                 'xyz': [-0.150688420258491, 0.0870000000000015, 0.0715000000000002],
                 'rpy': [-1.5707963267949, 0, 1.04719755119659]
             },
             {
-                'name': 'wheel_2_link.STL',
+                'name': 'link_wheel_2.STL',
                 'xyz': [0, -0.174, 0.0715],
                 'rpy': [-1.5707963267949, 0, -3.14159265358979]
             }
