878a557a74cca3847b1125dd585758b799a30b3a
Shehab <sattia@hello-robot.com>
2026-06-25 16:43:46 -0400

    Increased hold duration to START_BUTTON_HOLD_TIME_S

--- stretch4_body/core/gamepad_teleop.py
+++ stretch4_body/core/gamepad_teleop.py
@@ -415,7 +415,7 @@ class GamePadTeleop(Device):
 
         def on_select_hold():
             self.stow_robot()
-        self.select_button_counter.trigger_on_hold(2.0, on_select_hold)
+        self.select_button_counter.trigger_on_hold(START_BUTTON_HOLD_TIME_S, on_select_hold)
             
 
     def change_gripper_handedness(self, robot, *, do_motion:bool):
