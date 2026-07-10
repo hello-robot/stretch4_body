d27530812773f6e20a43a6e23cb2b35279483257
Ola Ghattas <oghattas@hello-robot.com>
2026-07-09 22:35:42 -0700

    update line viz to use stertch_server when available, remove cv2 import for nav2 to work

--- stretch4_body/core/hello_utils.py
+++ stretch4_body/core/hello_utils.py
@@ -10,7 +10,6 @@ import signal
 import pathlib
 import numbers
 import subprocess
-import cv2
 import sys, tty, termios
 import math
 from statistics import mean
@@ -735,6 +734,8 @@ def setup_uvc_camera(device_index, size=None, fps=None, format = None):
     """
     Returns Opencv capture object of the UVC video divice
     """
+    import cv2
+
     cap = cv2.VideoCapture(device_index)
     if format:
         fourcc_value = cv2.VideoWriter_fourcc(*f'{format}')
