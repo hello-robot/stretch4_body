96019d5bb0c03ab416ce7f2c39ae1a8c72a87604
Hello Robot Inc <hello-robot@my.hostname.example>
2026-07-28 14:09:02 -0700

    commented the gamepad singleton as it doesnt let me run gamepad even though there is no process locking it.

--- stretch4_body/tools/stretch_gamepad_teleop.py
+++ stretch4_body/tools/stretch_gamepad_teleop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from stretch4_body.core.gamepad_control_mappings import ControlMapping
-from stretch4_body.core.gamepad_controller import check_gamepad_teleop_singleton
+# from stretch4_body.core.gamepad_controller import check_gamepad_teleop_singleton
 from stretch4_body.core.gamepad_teleop import GamePadTeleop
 from stretch4_body.core.hello_utils import print_stretch_re_use
 
@@ -13,9 +13,9 @@ if __name__ == "__main__":
    parser.add_argument("-d", "--direct", help="Use direct API (no server)", action="store_true")
    args=parser.parse_args()
 
-   if not check_gamepad_teleop_singleton():
-      print("Gamepad teleop is already running!")
-      exit(1)
+   # if not check_gamepad_teleop_singleton():
+   #    print("Gamepad teleop is already running!")
+   #    exit(1)
    
    # Provide a helpful description of the controls for each mapping
    mappings = ControlMapping._get_cycleable_options()
