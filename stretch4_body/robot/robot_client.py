14d5ec9b66a58222309d5bcd13883aec4a7212c8
Shehab <sattia@hello-robot.com>
2026-08-26 17:20:44 -0400

    Disabled set_velocity for wrist joints temporarily (#158)
    * Disabled set_velocity for wrist joints until the implementation is complete.
    
    * Added the knockback to EndOfArmClient too

--- stretch4_body/robot/robot_client.py
+++ stretch4_body/robot/robot_client.py
@@ -1120,6 +1120,8 @@ class WristJointClient(SubsystemClient):
         a_r : float, optional
             Acceleration limit (rad/s^2).
         """
+        self.logger.error(f"Cannot send movement command. Joint {self.joint_name} does not support set_velocity.")
+        return False
         if not self.is_homed():
             self.logger.error(f"Cannot send movement command. Joint {self.joint_name} has not been homed.")
             return False
@@ -1371,6 +1373,8 @@ class EndOfArmClient(SubsystemClient):
         a_r : float, optional
             Acceleration limit (rad/s^2).
         """
+        self.logger.error(f"Cannot send movement command. Joint {joint} does not support set_velocity.")
+        return False
         if joint not in self.joints:
             self.logger.error(f"Joint {joint} not found in end of arm tool.")
             return False
